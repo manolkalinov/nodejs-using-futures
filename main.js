695cc5e6bdab1be9b9450170e1e1edf48a8f8cdd
Chris Mather <mather.chris@gmail.com>
2013-05-30 16:12:54 -0700

    wip

--- main.js
+++ main.js
@@ -11,9 +11,10 @@ function doAsyncWork (callback) {
   }, 3000);
 }
 
-// Using Fibers
+// Doing async work with 
 function doAsyncWorkWithFiber () {
   var fiber = Fiber.current;
+
   setTimeout(function () {
     fiber.run('result of work');
   }, 3000);
@@ -23,30 +24,15 @@ function doAsyncWorkWithFiber () {
   return results;
 }
 
-// Using Futures
-//  - call wait() on return value to yield to another fiber
-function doAsyncWorkWithFuture () {
-  var future = new Future;
-  setTimeout(function () {
-    future.return('result of work');
-  }, 3000);
-  return future;
-}
-
-
-// Wrapping with Futures when we don't control the underlying api
-//  - call wait() on return value to yield to another fiber
-function wrapDoAsyncWork () {
-  var future = new Future;
-  doAsyncWork(future.resolver());
-  return future;
-}
-
-function handleRequest (i) {
+var handleRequest = function (i) {
   Fiber(function () {
+
     print(i, 'handling request');
-    var results = wrapDoAsyncWork().wait();
+
+    var results = doAsyncWorkWithFiber();
+
     print(i, 'after doAsyncWorkCall with result ' + results);
+
   }).run();
 }
 
